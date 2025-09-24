e3e24e51f1cc6f4ecc088637c06c5a8ac238b3f4
antonBlagodarnyy <antonbgi98@gmail.com>
2025-09-24 15:45:50 +0200

    Init of both client and server

---
+++ ChatRoomWs/src/main/java/com/ChatRoomWS/ChatRoomWs/ChatRoomWsApplication.java
@@ -0,0 +1,13 @@
+package com.ChatRoomWS.ChatRoomWs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomWsApplication.class, args);
+	}
+
+}
