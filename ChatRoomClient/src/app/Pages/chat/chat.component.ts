19f27bc07a48d648172b473ee0f0373e634536da
antonBlagodarnyy <antonbgi98@gmail.com>
2025-09-25 15:08:48 +0200

    ws connection

--- ChatRoomClient/src/app/Pages/chat/chat.component.ts
+++ ChatRoomClient/src/app/Pages/chat/chat.component.ts
@@ -10,5 +10,15 @@ import { MessagesComponent } from '../../Components/messages/messages.component'
 export class ChatComponent implements OnInit {
   
   constructor(private chatService: ChatService) {}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.chatService.connect()?.subscribe({
+      next: (value) => {
+        //TODO Create a new message and push it to app-messages
+        console.log('message received: ' + value);
+      }, // Called whenever there is a message from the server.
+      error: (err) => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete'), // Called when connection is closed (for whatever reason).
+    });
+  }
 }
--- ChatRoomClient/src/app/Services/chat.service.ts
+++ ChatRoomClient/src/app/Services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { IMessage } from '../Interfaces/IMessage';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,16 @@ export class ChatService {
 
   connect() {
     return (this.ws = webSocket('ws://localhost:8081'));
+  }
+  sendMsg(msg: IMessage) {
+    if (this.ws) {
+      this.ws.subscribe();
+      this.ws.next({
+        from: msg.from,
+        text: msg.txt,
+      });
+      this.ws.complete();
+      this.ws.error({ code: 4000, reason: 'Smth broke' });
+    }
   }
 }
